ca589118bda03dee1a820e0f46b6792265948d9a
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-05 14:14:56 +0400

    practice

---
+++ Chapter 1- Getting Started with Python/Exercises/practice.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+age = input("What is your age: ")
+phone_number = input("What is your phone number: ")
+birthday = input("When is your birthday: ")
+home_address = input("What is your address: ")
+hobbies = input("What are your hobbies: ")
+
+print("Personal Identification", "\nName: " + name, "\nAge: " + str(age), "\nPhone Number: " + str(phone_number), "\nBirthday: " + birthday, 
+      "\nHome Address: " + home_address, "\nHobbies: " + hobbies)
