638304c2f54a7a7cc2059292fb2366d544f13611
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-12 00:49:44 +0800

    practice4.py

--- Chapter 1- Getting Started with Python/Exercises/practice4.py
+++ Chapter 1- Getting Started with Python/Exercises/practice4.py
@@ -3,4 +3,4 @@
 base = float(input("Enter the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 
-print("The area of the triangle is " , 0.5*base*height)
+print(f'The area of the triangle is: {0.5*base*height}')
