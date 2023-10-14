827bfb3e02d1ac3ed8acd8f0bf45d9d8b8ec9058
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-15 01:26:02 +0800

    exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -0,0 +1,7 @@
+#The amount of USB sticks that can be bought by the girl
+#Using the / or division operator which divides 50 from 6 and converting the data type of the value to an integer
+print(f'The girl can buy {int(50/6)} USB sticks.')
+
+#How many pounds the girl will be left with when USB sticks have been bought.
+#Using the % or modulo operator which divides 50 from 6 and returns a remainder 
+print(f'The girl will only have £{50%6} left.')
