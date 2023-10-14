6dbae683754a7879e457834f4f98a9d16021e0f5
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-15 01:25:34 +0800

    exercise4.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise4.py
@@ -0,0 +1,4 @@
+#A variable that has a value of my favorite number
+fav_number = 38
+#Using an f string to enclose the fav_number variable inside the print statement
+print(f'My favorite number is {fav_number}!')
