d37ed2ecc321f696e183afed4895aac0169451a2
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-21 19:31:45 +0800

    exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -0,0 +1,8 @@
+#A names list that stores a list of my friends
+names = ["Paolo", "Lanz", "Andre", "John"]
+
+#The following print statements access the elements inside the names list which uses positive indexing
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
