cde1c721186b7c93ba88370e5254be5845a4cdad
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-21 20:04:36 +0800

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,10 @@
+#My favorite mode tranportation list
+
+transport = ["SUV", "tricycle", "bike", "train", "RV"]
+
+#Print statements about the items in the favorite mode tranportation list
+print(f'I would like to own a Xpander {transport[0]}.')
+print(f'Back in the Philippines I sometimes ride a {transport[1]} when I am going to buy groceries at my local supermarket.')
+print(f'I like riding my {transport[2]} when I have some free time.')
+print(f'I always feel mesmerize by the view outside of a {transport[3]}.')
+print(f'I think staying in a {transport[4]} for summer vacation would be an amazing experience.')
