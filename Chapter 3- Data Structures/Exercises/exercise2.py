cf4c940d388edd127156419a9c39d8cda8e8940a
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-21 20:06:45 +0800

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,8 @@
+#A names list that stores a list of my friends
+names = ["Paolo", "Lanz", "Andre", "John"]
+
+#Print statements on greeting the person in the names list
+print(f'Good morning! {names[0]}')
+print(f'Good morning! {names[1]}')
+print(f'Good morning! {names[2]}')
+print(f'Good morning! {names[3]}')
