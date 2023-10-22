eea616116ac05772e3c10c70e05012d3b8915259
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-22 20:37:25 +0800

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,8 @@
+#A guests list that stores three people that I would like to invite to dinner
+
+guests = ["Bert", "Rudolfo", "Pele"]
+
+#Print statements on inviting each person of the guests list to a dinner
+print(f'Good day! {guests[0]} I would like to invite you to dinner.')
+print(f'Good day! {guests[1]} I would like to invite you to dinner.')
+print(f'Good day! {guests[2]} I would like to invite you to dinner.')
