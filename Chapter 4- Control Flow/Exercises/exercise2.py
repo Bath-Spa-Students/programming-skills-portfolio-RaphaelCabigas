029c69ad7eece59ab8dbbdfa0858fb7cfa19ecf6
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-30 00:45:13 +0800

    exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,23 @@
+# "GREEN" VERSION: +5 POINTS
+
+# A alien_color variable with a string value "green"
+alien_color = "green"
+
+# An if-else statement testing if the alien_color variable has the string "green"
+# If it is "green" then it will print +5 points
+# Else it will print +10 points
+if alien_color == "green":
+    print("ALIEN SHOT! +5 points earned") # Since the value of the variable is "green" it will execute the if statement
+else:
+    print("+10 points earned")
+
+# "YELLOW" VERSION: +10 POINTS
+
+# A alien_color variable with a string value "yellow"
+alien_color = "yellow"
+
+# The same if-else statement as before but for "yellow"
+if alien_color == "green":
+    print("ALIEN SHOT! +5 points earned")
+else:
+    print("+10 points earned") # Since the value of the variable is "yellow" and there is no condition for "yellow" it will execute the else statement
