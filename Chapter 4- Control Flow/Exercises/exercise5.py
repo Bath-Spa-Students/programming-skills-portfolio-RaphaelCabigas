548e9d741e35068b4fe306dd9f23d47a04893659
RaphaelCabigas <raph.ccc3@gmail.com>
2023-10-30 00:45:22 +0800

    exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,20 @@
+# A list of my favorite fruits
+favorite_fruits = ["orange", "lemon", "rambutan"]
+
+# 5 different if statements checking certain fruits inside of the favorite_fruits list
+# If a certain fruit is inside the list it will print a statement 
+# If a certain fruit is not inside of the list it will not execute
+if "orange" in favorite_fruits:
+    print("Since you like orange there must be a lot of vitamin C in you.")
+
+if "lemon" in favorite_fruits:
+    print("WHAT!! DO YOU EAT LEMONS EVERYDAY??")
+
+if "rambutan" in favorite_fruits:
+    print("You like rambutan?! Now that's rare.")
+    
+if "strawberry" in favorite_fruits:
+    print("Strawberry is really liked by many huh.")
+    
+if "kiwi" in favorite_fruits:
+    print("I did not know you like kiwi.")
