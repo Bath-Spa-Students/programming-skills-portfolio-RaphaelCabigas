bafec3275d1d01561345fc84ccc589b3fe8bb02e
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-03 18:46:40 +0800

    practice3.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice3.py
@@ -0,0 +1,11 @@
+# Write a python program that takes an input 5 from user and then type cast those values to string, int
+# and float in the separate variables. Print all the variables.
+
+x = str(5)  # str type cast makes the value 5 into string
+y = int(5)  # int type cast makes the value 5 into integer
+z = float(5)  # float type cast makes the value 5 into number with decimal point
+
+# prints the x, y, z variables
+print(x)
+print(y)
+print(z)
