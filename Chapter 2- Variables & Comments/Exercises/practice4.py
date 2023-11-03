42ce94ae86a59ed6660a0fde2aca8fe73335c13c
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-03 18:46:50 +0800

    practice4.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice4.py
@@ -0,0 +1,9 @@
+# Write a python program that stores an integer and string value to variables x and y. Print the type of
+# variable x and y.
+
+x = "School"  # (x) variable stores a string value of "School"
+y = 20  # (y) variable stores an integer of 20
+
+# prints the data type that is stored in the (x and y) variables
+print(type(x))
+print(type(y))
