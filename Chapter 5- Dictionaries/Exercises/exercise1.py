82d319fe55739ff0c257896d5ffb12b5f3f72be8
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-03 18:40:07 +0800

    exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+# A dictionary named personal_information of someone
+# With keys of first name, last name, age, and city the person lives in
+personal_information = {'First Name': 'Rudolph',
+                        'Last Name': 'Mclain',
+                        'Age': '25',
+                        'City': 'New York'}
+
+# prints everything inside of personal_information
+print(personal_information)
