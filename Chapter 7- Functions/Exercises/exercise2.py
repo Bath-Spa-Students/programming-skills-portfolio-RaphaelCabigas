b4af1f081d9772c4e53421081c94b6a8d98259e1
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-08 21:06:09 +0800

    exercise2.py

---
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -0,0 +1,11 @@
+# This is a favorite_book function in which we use def to define on what the function is going to do
+# With a parameter that is named title
+def favorite_book(title):
+    # The function stores this print statement with a f string that has the title parameter
+    print(f'My new favorite book is {title}')
+
+
+# The title parameter is set to our desired favorite book
+favorite_book("The Little Prince")
+
+# The code within these functions called will be executed in which prints the message
