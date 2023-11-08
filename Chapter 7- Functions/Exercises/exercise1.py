0e474dbf1cce4766f24db2dfe4aa857fc31eeacd
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-08 21:06:01 +0800

    exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+# This is a display_message function in which we use def to define on what the function is going to do
+def display_message():
+    # The display_message function stores this print statement
+    print("In this chapter of Python we will learn functions.")
+
+
+display_message()
+
+# This will run the function and the code within it will be executed in which prints the message
