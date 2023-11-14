7f8401db88a9b86df2b59d3b9a1b3cde226a5a2c
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-15 02:15:08 +0800

    practice1.py

---
+++ Chapter 7- Functions/Exercises/practice1.py
@@ -0,0 +1,9 @@
+# This is a function_sum in which we use def to define on what the function is going to do
+# with two parameters of a and b
+def function_sum(a, b):
+    # The print statement adds both the values of a and b with the help of a plus sign math operator for addition
+    print(a+b)
+
+
+# We call the function_sum and use keyword arguments and assigning a value of 5 to a and a value of 4 to b
+function_sum(b=5, a=4)
