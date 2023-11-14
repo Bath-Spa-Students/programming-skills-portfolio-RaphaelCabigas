902ac7e0741edd8a1ab8721d2d3c64da91e0fc5b
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-15 02:23:55 +0800

    practice2.py

---
+++ Chapter 4- Control Flow/Exercises/practice2.py
@@ -0,0 +1,13 @@
+# A a variable asking for an input and a int() which converts the input to an integer
+a = int(input("Enter a number greater or less than 10: "))
+
+# An if-else statement for the a variable
+# if a is less than 10 it will the b variable will have a value string letter O
+# Else b variable will have a value of 99
+if a < 10:
+    b = "O"
+else:
+    b = 99
+
+# prints the value of b variable
+print(b)
