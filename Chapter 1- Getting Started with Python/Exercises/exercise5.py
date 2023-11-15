731ca1462bf66edafe019aa78c5a19d87079f3d2
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-16 00:09:58 +0800

    exercise5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -1,2 +1,5 @@
+# A radius variable asking input for the radius of a circle and converts the input to float
 radius = float(input("Enter the radius of the circle: "))
+# A print statement with an f string to store the formula for the area of a circle and the radius input
+# Which is 3.14 multiply by radius and radius using the * multiply sign operator
 print(f'The area of the circle is: {3.14*radius*radius}')
