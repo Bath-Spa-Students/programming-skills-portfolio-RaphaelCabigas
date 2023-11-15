90aae14747397aa70ff0d3c3b4a760daad4d40b4
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-15 23:59:46 +0800

    exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -1,3 +1,5 @@
+# A print statement with triple double quotes that creates multiple lines for a string
+# Meaning you can add any number of spaces as you like to make the string the desired look that you want
 print("""Twinkle, twinkle, little star,
     How I wonder what you are! 
         Up above the world so high,   		
