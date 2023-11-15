f97122300cfb93cb95f35fd6aba4bf1182877ca9
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-16 00:02:56 +0800

    exercise2.py

--- Chapter 1- Getting Started with Python/Exercises/exercise2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise2.py
@@ -1,3 +1,4 @@
+# Importing a sys module gains access to specific functions
 import sys
+# With an f string, sys.version checks the version of the Python Interpreter
 print(f'Python Version: {sys.version}')
-print(f'Version Info: {sys.version_info}')
