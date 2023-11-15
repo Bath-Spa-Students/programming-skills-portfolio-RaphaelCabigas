33704ac2f6091beb3951962446c11fce6f632f86
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-16 00:08:10 +0800

    exercise4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -1,5 +1,7 @@
+# a, b, and c variables storing string values
 a = "Hello "
 b = "Bath Spa "
 c = "University"
 
+# prints and concatenates the three variables into one with the help of a + plus sign operator
 print(a + b + c)
