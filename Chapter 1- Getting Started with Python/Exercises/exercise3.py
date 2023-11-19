2a3f16393e757ec0ea74ac65b4f0b3efe2e6f412
RaphaelCabigas <raph.ccc3@gmail.com>
2023-11-19 09:31:26 +0400

    exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise3.py
@@ -1,9 +1,6 @@
 # Importing a datetime module that gains access to specific functions for date and time
 import datetime
-# A today variable that gets the current date and time
-# The FIRST datetime. is the name module we are using
-# The SECOND datetime. is the datetime class that stores things about date and time like year and hour
-# today() gets the current date and time
+# A today variable that stores the command to get the current date and time
 today = datetime.datetime.today()
 # With an f string, stores the today variable which shows the current date and time
 print(f'The current date and time today is {today}')
